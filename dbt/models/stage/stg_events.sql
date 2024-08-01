7ecba59263f03b1886b9cd53ca0641bd0b557bd6
vecpub <alex@vecpub.ai>
2024-08-01 18:07:38 -0400

    Add initial event and place transforms

---
+++ dbt/models/stage/stg_events.sql
@@ -0,0 +1,23 @@
+with tm_source as (
+select distinct 
+	event.name as event_name,
+    'ticketmaster' as source,
+    md5(concat(event.name, '|' ,dates__start__local_date)) as source_key,
+    dates__start__date_time as start_datetime,
+    event.id as external_id,
+    event.url as ticket_url,
+    dates__status__code as ticket_status,
+    venue.name as venue_name,
+    venue.id as source_venue_id,
+    md5(concat(venue.name, '|' ,venue.city__name, venue.state__state_code)) as venue_source_key,
+    classification.segment__name as segment_name,
+    classification.genre__name as genre_name,
+    classification.sub_genre__name as sub_genre_name
+
+from pl_ticketmaster.stg_ticketmaster event
+left join pl_ticketmaster.stg_ticketmaster___embedded__venues venue on event._dlt_id = venue._dlt_parent_id
+left join pl_ticketmaster.stg_ticketmaster__classifications classification on event._dlt_id = classification._dlt_parent_id
+
+--order by 1
+)
+select * from tm_source
---
+++ dbt/models/stage/stg_places.sql
@@ -0,0 +1,19 @@
+
+
+with tm_source as (
+select distinct name, 
+    'ticketmaster' as source,
+    md5(concat(name, '|' ,city__name, state__state_code)) as source_key,
+    id as external_id,
+    location__latitude as latitude,
+    location__longitude as longitude,
+    address__line1 as address_line_1,
+    address__line2 as address_line_2,
+    postal_code,
+    city__name as city_name,
+    state__state_code as state_code,
+country__country_code as country_code,
+social__twitter__handle as twitter_handle
+from pl_ticketmaster.stg_ticketmaster___embedded__venues order by 1
+)
+select * from tm_source
